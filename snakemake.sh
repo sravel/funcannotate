9aa4214e3a5cf8db4d2fa6f1ee8edf91017816b1
droc <gaetan.droc@cirad.fr>
2022-03-21 17:11:12 +0100

    Update snakemake.sh
    Typo

--- snakemake.sh
+++ snakemake.sh
@@ -16,5 +16,5 @@ mkdir -p logs/slurm/
 # snakemake  --cluster-config cluster_config.yml  --configfile config.yaml --jobs 200 --rerun-incomplete
 #snakemake  --cluster-config cluster_config.yml  --configfile config.yaml --jobs 200 --printshellcmds --dryrun --use-envmodules  --cluster "sbatch  -p {cluster.partition} --ntasks {cluster.ntasks} --mem-per-cpu={cluster.mem-per-cpu} -c {cluster.cpus-per-task} -e {cluster.error} -o {cluster.output}"
 # snakemake  --cluster-config cluster_config.yml  --configfile config.yaml --jobs 200 --unlock  --use-envmodules
-snakemake  --cluster-config cluster_config.yml  --configfile config.yaml --jobs 2 --dag  --use-envmodules | dot -Tpdf > dag.pdf
-#snakemake --cluster-config cluster_config.yml  --configfile config.yaml --jobs 200  --use-envmodules  --cluster "sbatch  -p {cluster.partition} --ntasks {cluster.ntasks} --mem-per-cpu={cluster.memory} -c {cluster.cpus-per-task} -e {cluster.error} -o {cluster.output}"
+#snakemake  --cluster-config cluster_config.yml  --configfile config.yaml --jobs 2 --dag  --use-envmodules | dot -Tpdf > dag.pdf
+snakemake --cluster-config cluster_config.yml  --configfile config.yaml --jobs 200  --use-envmodules  --cluster "sbatch  -p {cluster.partition} --ntasks {cluster.ntasks} --mem-per-cpu={cluster.memory} -c {cluster.cpus-per-task} -e {cluster.error} -o {cluster.output}"
